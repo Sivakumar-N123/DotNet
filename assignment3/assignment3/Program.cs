4c3a13d3f915b6b49c7112e9e049c78833b79626
1sivakumar <91527131+1sivakumar@users.noreply.github.com>
2022-07-18 10:46:23 +0530

    assignment 3

---
+++ assignment3/assignment3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstValue;
+            Console.WriteLine("Enter the first value");
+            firstValue = Console.ReadLine();
+            string secondValue;
+            Console.WriteLine("Enter the second value");
+            secondValue = Console.ReadLine();
+            char[] temp = firstValue.ToLower().ToCharArray();
+            char[] temp1 = secondValue.ToLower().ToCharArray();
+            Array.Sort(temp);
+            Array.Sort(temp1);
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] != temp1[i])
+                {
+                    Console.WriteLine("Both string are not anagram string");
+                }
+            }
+            Console.WriteLine("Both string are  anagram string");
+            Console.ReadLine();
+        }
+    }
+}
